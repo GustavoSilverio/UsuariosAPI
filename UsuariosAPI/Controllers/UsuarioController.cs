97622481652fa004b643c98be36df9fe31fb5ec7
GustavoSilverio <gustavosk290905@gmail.com>
2024-03-09 19:28:11 -0300

    Add project files.

---
+++ UsuariosAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsuariosAPI.Models.DTO;
+
+namespace UsuariosAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CadastraUsuario([FromBody] CreateUsuarioDTO usuarioDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
